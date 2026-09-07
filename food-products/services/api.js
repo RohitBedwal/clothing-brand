786a34e9a20c2be839fad653e13929734b87e809
“rohit” <rohitbedwal09@gmail.com>
2026-09-07 23:37:53 +0530

    added env

--- food-products/services/api.js
+++ food-products/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = '/api';
+const API_BASE = import.meta.env.VITE_API_URL || '/api';
 
 const request = async (url, options = {}) => {
   const { headers: customHeaders, ...rest } = options;
